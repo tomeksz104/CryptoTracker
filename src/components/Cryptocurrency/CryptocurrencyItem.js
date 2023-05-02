7e7dca9c843909fa8ab51e03dcf6fe8a2941d251
tomeksz104 <48735947+tomeksz104@users.noreply.github.com>
2023-05-02 20:58:17 +0200

    Updated CryptocurrencyItem component with additional functionality and UI elements

--- src/components/Cryptocurrency/CryptocurrencyItem.js
+++ src/components/Cryptocurrency/CryptocurrencyItem.js
@@ -1,9 +1,12 @@
 import { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 import { updateCryptocurrencyInNewCurrency } from "../..//utils/cryptoUtils";
 import empty from "../../assets/cryptocurrency-icons/empty.svg";
 import { ReactComponent as PlusSvg } from "../../assets/svg/plus.svg";
+import { ReactComponent as CaretDown } from "../../assets/svg/caret-down.svg";
+import { ReactComponent as CaretUp } from "../../assets/svg/caret-up.svg";
 
 const CryptocurrencyItem = (props) => {
   const imgRef = useRef(null);
@@ -11,19 +14,22 @@ const CryptocurrencyItem = (props) => {
     (state) => state.currency
   );
 
-  console.log(currentCurrency);
+  const circulatingSupplyPercentage = isFinite(
+    props.cryptocurrency.supply / props.cryptocurrency.maxSupply
+  )
+    ? (
+        (props.cryptocurrency.supply / props.cryptocurrency.maxSupply) *
+        100
+      ).toFixed() + "%"
+    : "-";
 
-  if (currentCurrency !== "USD" && currentCurrencyRate !== 0) {
-    const cryptocurrency = updateCryptocurrencyInNewCurrency(
-      props.cryptocurrency,
-      currentCurrency,
-      currentCurrencyRate
-    );
-  } else {
-    const cryptocurrency = props.cryptocurrency;
-  }
+  const cryptocurrency = updateCryptocurrencyInNewCurrency(
+    props.cryptocurrency,
+    currentCurrency,
+    currentCurrencyRate
+  );
 
-  console.log(props.cryptocurrency);
+  console.log(cryptocurrency);
 
   useEffect(() => {
     const fetchSvg = async () => {
@@ -39,6 +45,20 @@ const CryptocurrencyItem = (props) => {
     fetchSvg();
   }, [props.cryptocurrency.symbol]);
 
+  const changePercent24hButton = isNaN(cryptocurrency.changePercent24Hr) ? (
+    "-"
+  ) : cryptocurrency.changePercent24Hr > 0 ? (
+    <span class="text-white bg-green-500 font-medium rounded-md text-xs px-3 py-1.5 text-center inline-flex items-center ml-3">
+      <CaretUp className="fill-white w-3 h-3" />
+      {cryptocurrency.changePercent24Hr}%
+    </span>
+  ) : (
+    <span class="text-white bg-red-500 font-medium rounded-md text-xs px-3 py-1.5 text-center inline-flex items-center ml-3">
+      <CaretDown className="fill-white w-3 h-3" />
+      {cryptocurrency.changePercent24Hr}%
+    </span>
+  );
+
   return (
     <>
       <div className="flex items-center justify-between mt-8">
@@ -46,15 +66,18 @@ const CryptocurrencyItem = (props) => {
           <img
             ref={imgRef}
             src={empty}
-            alt={`Logo ${props.cryptocurrency.name}`}
+            alt={`Logo ${cryptocurrency.name}`}
             width={32}
             height={32}
           />
           <h2 className="text-4xl font-semibold leading-none sm:text-4xl xl:max-w-3xl text-slate-700 dark:text-white">
-            {props.cryptocurrency.name}
+            {cryptocurrency.name}
           </h2>
-          <span className="flex items-center text-xs font-medium px-3 py-2 space-x-2 bg-slate-400/10 rounded-md text-neutral-800 dark:text-neutral-300">
-            {props.cryptocurrency.symbol}
+          <span
+            className="flex items-center text-xs font-medium bg-slate-400/10 rounded-md text-neutral-800 dark:text-neutral-300"
+            style={{ padding: "5px 8px" }}
+          >
+            {cryptocurrency.symbol}
           </span>
         </div>
         <a
@@ -68,7 +91,92 @@ const CryptocurrencyItem = (props) => {
           </span>
         </a>
       </div>
-      <div></div>
+      <div className="flex items-center mt-3">
+        <span
+          class="bg-slate-400/30 font-medium rounded-md text-xs dark:text-white"
+          style={{ padding: "5px 8px" }}
+        >
+          Rank #{cryptocurrency.rank}
+        </span>
+        <Link
+          to={cryptocurrency.explorer}
+          target="_blank"
+          class="bg-slate-400/10 hover:bg-slate-400/20 font-medium rounded-md text-xs text-center inline-flex items-center ml-3 dark:text-white"
+          style={{ padding: "5px 8px" }}
+        >
+          Explorer
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-4 h-4 ml-1"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+            />
+          </svg>
+        </Link>
+      </div>
+
+      <div class="mt-5 grid gap-6 lg:w-full lg:grid-cols-4 border-y border-gray-100 dark:border-gray-700 py-5">
+        <div class="border-r border-gray-100 dark:border-gray-700">
+          <h3 className="text-sm text-slate-500 dark:text-slate-400">
+            {cryptocurrency.name} Price ({cryptocurrency.symbol})
+          </h3>
+          <span className="text-sm font-semibold text-slate-700 dark:text-white">
+            {cryptocurrency.priceUsd}
+            {changePercent24hButton}
+          </span>
+        </div>
+        <div class="border-r border-gray-100 dark:border-gray-700">
+          <h3 className="text-sm text-slate-500 dark:text-slate-400">
+            Market Cap
+          </h3>
+          <span className="text-sm font-semibold text-slate-700 dark:text-white">
+            {cryptocurrency.marketCapUsd}
+          </span>
+        </div>
+        <div class="border-r border-gray-100 dark:border-gray-700">
+          <h3 className="text-sm text-slate-500 dark:text-slate-400">Volume</h3>
+          <span className="text-sm font-semibold text-slate-700 dark:text-white">
+            {cryptocurrency.volumeUsd24Hr}
+          </span>
+        </div>
+        <div>
+          <h3 className="text-sm text-slate-500 dark:text-slate-400">
+            Circulating Supply
+          </h3>
+          <div className="flex justify-between text-sm">
+            <span className="font-semibold text-slate-700 dark:text-white">
+              {cryptocurrency.supply}
+            </span>
+            <span className="text-slate-500 dark:text-slate-400">
+              {circulatingSupplyPercentage}
+            </span>
+          </div>
+          <div class="w-full h-2 bg-slate-400/10 rounded-full mt-3">
+            <div
+              class="h-full text-center text-xs text-white bg-slate-400/30 rounded-full"
+              style={{ width: circulatingSupplyPercentage }}
+            ></div>
+          </div>
+
+          <div className="flex justify-between text-sm mt-3">
+            <span className="text-slate-500 dark:text-slate-400">
+              Max supply:
+            </span>
+            <span className="font-semibold text-slate-700 dark:text-white">
+              {!isNaN(parseFloat(cryptocurrency.maxSupply))
+                ? cryptocurrency.maxSupply
+                : "-"}
+            </span>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
