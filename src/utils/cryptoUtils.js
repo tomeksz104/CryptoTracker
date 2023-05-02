5a3defbe21e623370826acc5e463c210d4d62f01
tomeksz104 <48735947+tomeksz104@users.noreply.github.com>
2023-05-02 12:11:50 +0200

    Refactored updateCryptocurrencyInNewCurrency function to use conditional logic to format when rateUsd is 0

--- src/utils/cryptoUtils.js
+++ src/utils/cryptoUtils.js
@@ -6,28 +6,42 @@ export function updateCryptocurrencyInNewCurrency(
   rateUsd
 ) {
   const priceUsd = parseFloat(cryptocurrency.priceUsd.replace(/[$,]/g, ""));
-  const priceInNewCurrency = priceUsd / rateUsd;
-
   const marketCapUsd = parseFloat(
     cryptocurrency.marketCapUsd.replace(/[$,]/g, "")
   );
-  const marketCapInNewCurrency = marketCapUsd / rateUsd;
-
   const volumeUsd24Hr = parseFloat(
     cryptocurrency.volumeUsd24Hr.replace(/[$,]/g, "")
   );
-  const volume24HrInNewCurrency = volumeUsd24Hr / rateUsd;
 
-  return {
-    ...cryptocurrency,
-    price: formatCurrency(priceInNewCurrency, symbol, "en", false, {
-      decimalPlaces: 2,
-    }),
-    marketCap: formatCurrency(marketCapInNewCurrency, symbol, "en", false, {
-      decimalPlaces: 2,
-    }),
-    volume24Hr: formatCurrency(volume24HrInNewCurrency, symbol, "en", false, {
-      decimalPlaces: 2,
-    }),
-  };
+  if (rateUsd !== 0) {
+    const priceInNewCurrency = priceUsd / rateUsd;
+    const marketCapInNewCurrency = marketCapUsd / rateUsd;
+    const volume24HrInNewCurrency = volumeUsd24Hr / rateUsd;
+
+    return {
+      ...cryptocurrency,
+      price: formatCurrency(priceInNewCurrency, symbol, "en", false, {
+        decimalPlaces: 2,
+      }),
+      marketCap: formatCurrency(marketCapInNewCurrency, symbol, "en", false, {
+        decimalPlaces: 2,
+      }),
+      volume24Hr: formatCurrency(volume24HrInNewCurrency, symbol, "en", false, {
+        decimalPlaces: 2,
+      }),
+    };
+  } else {
+    return {
+      ...cryptocurrency,
+      priceUsd: formatCurrency(priceUsd, symbol, "en", false, {
+        decimalPlaces: 2,
+      }),
+      marketCapUsd: formatCurrency(marketCapUsd, symbol, "en", false, {
+        decimalPlaces: 2,
+      }),
+      volumeUsd24Hr: formatCurrency(volumeUsd24Hr, symbol, "en", false, {
+        decimalPlaces: 2,
+      }),
+    };
+  }
 }
