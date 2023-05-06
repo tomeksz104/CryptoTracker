968e1752096e561f68643eb30d3bc42c07f869ae
tomeksz104 <48735947+tomeksz104@users.noreply.github.com>
2023-05-06 14:29:32 +0200

    Updated PageContent component to accept custom classes

--- src/components/Layout/PageContent.js
+++ src/components/Layout/PageContent.js
@@ -1,6 +1,10 @@
-const PageContent = ({ children }) => {
+const PageContent = ({ classes, children }) => {
+  const containerClasses = classes
+    ? `container mx-auto ${classes}`
+    : "container mx-auto my-8";
+
   return (
-    <div className="container mx-auto my-8">
+    <div className={containerClasses}>
       <div className="relative overflow-visible">{children}</div>
     </div>
   );
